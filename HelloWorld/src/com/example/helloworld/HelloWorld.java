a0306d686e3c7a015e8fe19c24ca3262970215de
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-03 22:39:57 +0200

    Creating project into a module

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Nacho on 3/10/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
