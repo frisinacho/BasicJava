321367b2b0f164033a787ec432ad3f642416a4fa
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-04 13:11:57 +0200

    Adding logic OR

--- IfElse/src/com/example/ifelse/IfElse.java
+++ IfElse/src/com/example/ifelse/IfElse.java
@@ -7,7 +7,7 @@ public class IfElse {
     public static void main(String[] args){
         int number = 8;
 
-        if (number > 5 && number < 10) {
+        if (number > 5 || number < 10) {
             System.out.println("The number is greater than 5");
         }else{
             System.out.println("The number is lower or equal than 5");
