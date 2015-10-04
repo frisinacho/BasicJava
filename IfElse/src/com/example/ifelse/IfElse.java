4d94e1b97af5863dd8f952a5684c2b1835be22e9
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-04 13:07:52 +0200

    Adding condition

--- IfElse/src/com/example/ifelse/IfElse.java
+++ IfElse/src/com/example/ifelse/IfElse.java
@@ -5,6 +5,10 @@ package com.example.ifelse;
  */
 public class IfElse {
     public static void main(String[] args){
+        int number = 6;
 
+        if (number > 5) {
+            System.out.println("The number is greater than 5");
+        }
     }
 }
