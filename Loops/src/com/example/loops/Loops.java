0b39cfe54985eb73e85e7f3e75e1865acdf803e2
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-06 17:47:28 +0200

    For Loop

--- Loops/src/com/example/loops/Loops.java
+++ Loops/src/com/example/loops/Loops.java
@@ -6,5 +6,10 @@ package com.example.loops;
 public class Loops {
     public static void main(String[] args){
         int wagons[] = new int[5];
+        int i;
+
+        for (i = 0; i < 5; i++){
+
+        }
     }
 }
