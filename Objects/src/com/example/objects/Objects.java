24959d65dca074cb3b98f51d194e6487872fa361
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-08 15:36:22 +0200

    New instance for a train

--- Objects/src/com/example/objects/Objects.java
+++ Objects/src/com/example/objects/Objects.java
@@ -7,5 +7,7 @@ public class Objects {
     public static void main(String[] args){
         Train first = new Train();
         System.out.println(first.machinist);
+
+        Train second = new Train("Nacho", 25);
     }
 }
