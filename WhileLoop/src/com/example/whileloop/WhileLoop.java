7711fddb5f156a850be90493aa4b2d6eec0be29f
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-08 13:15:11 +0200

    Bring back the for loop for filling the seats

--- WhileLoop/src/com/example/whileloop/WhileLoop.java
+++ WhileLoop/src/com/example/whileloop/WhileLoop.java
@@ -6,6 +6,14 @@ package com.example.whileloop;
 public class WhileLoop {
     public static void main(String[] args){
         int wagons[][] = new int[5][2];
+        int i, j;
 
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 2; j++) {
+                if (j == 0) wagons[i][j] = (i + 1) * 10;
+                if (j == 1) wagons[i][j] = wagons[i][0] / 2;
+            }
+        }
+        System.out.println("There are " + wagons[0][1] + " passengers at the start.");
     }
 }
