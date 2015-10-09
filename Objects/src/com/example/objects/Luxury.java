5d517e2aae5b62ea2b99569bf8e1ed57e3bdc2bb
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-09 13:40:50 +0200

    Basic var

--- Objects/src/com/example/objects/Luxury.java
+++ Objects/src/com/example/objects/Luxury.java
@@ -4,5 +4,6 @@ package com.example.objects;
  * Created by Nacho on 9/10/15.
  */
 public class Luxury extends Train {
-
+    String waiter;  // Name of the waiter
+    boolean bar;    // Open or Close
 }
